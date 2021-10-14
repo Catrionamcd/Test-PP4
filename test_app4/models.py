d6303d6aaa40286ed700f0b833559334d7ea954b
Catrionamcd <catrionamcd@gmail.com>
2021-10-14 19:53:59 +0000

    Displaying channels

--- test_app4/models.py
+++ test_app4/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+#  Model for the Channel
+
+class Channel(models.Model):
+    topic = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="channel_posts")
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.topic
--- test_app4/views.py
+++ test_app4/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+# from django.views.generic import TemplateView
+from django.views import generic
+from .models import Channel
 
-# Create your views here.
+
+# class HomeView(TemplateView):
+#     template_name = 'base.html'
+
+
+class ChannelList(generic.ListView):
+    model = Channel
+    queryset = Channel.objects.filter(status=1).order_by("-created_on")
+    template_name = "index.html"
