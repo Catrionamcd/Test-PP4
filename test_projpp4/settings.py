6e834c53d244fe1c4f18cbfc8ace4f46d887df63
Catrionamcd <catrionamcd@gmail.com>
2021-10-19 15:51:49 +0000

    Change settings.py file for debug=developmet

--- test_projpp4/settings.py
+++ test_projpp4/settings.py
@@ -31,7 +31,13 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = development
+
+# if development:
+#     ALLOWED_HOSTS = ['localhost']
+# else:
+#     ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 ALLOWED_HOSTS = ['test-pp4.herokuapp.com', 'localhost']
 
